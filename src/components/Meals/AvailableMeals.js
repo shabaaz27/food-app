988b8980f06cd1916891b7df3030b7df2c913c4b
shabaaz27 <khanshabaaz05@gmail.com>
2022-06-26 20:41:47 +0530

    GetData from firebase Realdatabase using fetch

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,14 +1,71 @@
-import React from 'react'
+import React, { useEffect ,useLayoutEffect,useState} from 'react'
 import { DUMMY_MEALS } from './dummyMeal'
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card'
 import MealItem from '../MealItem/MealItem'
+import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
 
 const AvailableMeals = () => {
-    const mealList = DUMMY_MEALS.map((meal)=>{
+  const [meals,setMealsData] = useState([])
+  const [isLoading,setIsLoading] = useState(true)
+  const [httpError,setHttpError] = useState()
+ 
+  useLayoutEffect(()=>{   
+      const fetchMeals = async ()=>{    
+       const response = await fetch('https://instant-ground-308109-default-rtdb.firebaseio.com/meals.json')
+       if(!response.ok){
+            throw new Error('Something went wrong ')        
+
+       }
+       
+
+       const responseData = await response.json()
+       const loadedMeals = []
+          for(const key in responseData){
+            const res = responseData[key]
+            loadedMeals.push(
+            {
+              id:key,
+              name:res.name,
+              description:res.description,
+              price:res.price
+            }
+          )
+          }
+          console.log("loadedMeals====>",loadedMeals)
+          setMealsData(loadedMeals)
+          setIsLoading(false)
+
+      }
+      
+      fetchMeals().catch((error)=>{
+        setIsLoading(false)
+        setHttpError(error.message)
+       
+      })
+      
+
+    },[])
+
+
+    if(isLoading){
+      return <section className={classes.mealsLoading}> 
+        <p>Loading.....</p>
+      </section>
+    }
+    if(httpError){
+      return <section className={classes.mealsError}> 
+      <p>{httpError}</p>
+    </section>
+    }
+
+
+    const mealList = meals.map((meal)=>{
         return<MealItem  id={meal.id} key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
     })
-  return <section className={classes.meals}>
+  return(
+    <ErrorBoundary>
+    <section className={classes.meals}>
     <Card>
       <ul>
           {
@@ -17,6 +74,7 @@ const AvailableMeals = () => {
       </ul>
       </Card>
   </section>
-}
+  </ErrorBoundary>
+)}
 
 export default AvailableMeals
